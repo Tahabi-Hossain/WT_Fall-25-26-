5700b2e0d3784c2cb77dbb2858052eb6132ee8b3
Tahabi-Hossain <tahabihossain714@gmail.com>
2025-11-03 22:51:12 +0600

    First commit

---
+++ 22-47610-2 lab task 1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        22-47610-2 LT1
+</title>
+</head>
+<body>
+    
+    <center><h1 style="color:Red;border-bottom: 3px solid red; width: 100%;">Student Registration Information</h1></center>
+First Name<br>
+<Input type="text"style="width:250px;height:20px;"><br><br>
+Last Name<br>
+<Input type="text"style="width:250px;height:20px;"><br><br>
+Student ID<br>
+<Input type="number"style="width:250px;height:20px;"><br><br>
+Program/Major<br>
+<select>
+    <option value="Text"style="width:250px;height:20px;"><br> B.Sc in CSE</option>
+    <option value="Text"style="width:250px;height:20px;"><br> B.Sc in EEE</option>
+</select><br><br>
+Phone<br>
+<Input type="number"style="width:250px;height:20px;"><br><br>
+
+University Email<br>
+<Input type="text"style="width:250px;height:20px;"><br><br>
+
+Create Password<br>
+<Input type="number"style="width:250px;height:20px;"><br><br>
+
+Confirm Password<br>
+<Input type="number"style="width:250px;height:20px;"><br><br>
+
+Semester Selection:<br>
+<Input type="radio">Summer 2024 <Input type="radio">Fall 2024<Input type="radio">Spring 2025<Input type="radio">other/special<br><br>
+
+Request Credit Load (Max 15)<br>
+<Input type="number"style="width:250px;height:20px;"><br><br>
+
+<h2 style="color:Red;border-bottom: 3px solid red; width: 100%;">Course Selection</h2>
+
+<Input type="Checkbox">MATH 1201(Calculus1) <Input type="Checkbox">CS 2105(Data Structures)<Input type="Checkbox">ECON 1001(Microeconomics)<br>
+<Input type="Checkbox">PHY 1102(Physics Lab)<br><br>
+
+Comment/Special Requests<br>
+<Input type="text"style="width:450px;height:40px;"><br><br>
+
+<center><input type="submit"></center>
+
+</body>
+</html>
+
+
