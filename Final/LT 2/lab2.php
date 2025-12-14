f7215198c9303108201785dc32ab27082633fd2e
Tahabi-Hossain <tahabihossain714@gmail.com>
2025-12-15 00:09:38 +0600

    new commit

--- Final/LT 2/lab2.php
+++ Final/LT 2/lab2.php
@@ -1,92 +1,65 @@
 <!DOCTYPE html>
 <html>
 <head><title>PHP Code</title></head>
-
+ 
 <body>
-<h1>This is our 1st php code </h1>
-
+<h1>This is our 1st php code </h1>    
+ 
+ 
 <?php
-
-$name = "";
-$age = "";
-$email = "";
-
-$nameerror = "";
-$ageerror = "";
-$emailerror = "";
-
-// IF FORM SUBMITTED
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    // ---------------- NAME VALIDATION ----------------
-    if (empty($_POST["name"])) {
-        $nameerror = "Name is required";
-    } else {
-        $name = test_input($_POST["name"]);
-        if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
-            $nameerror = "Only letters and white space allowed";
-        }
-    }
-
-    // ---------------- AGE VALIDATION ----------------
-    if (empty($_POST["age"])) {
-        $ageerror = "Age is required";
-    } else {
-        $age = test_input($_POST["age"]);
-        if ($age < 1 || $age > 120) {
-            $ageerror = "Enter a valid age";
-        }
-    }
-
-    // ---------------- EMAIL VALIDATION ----------------
-    if (empty($_POST["email"])) {
-        $emailerror = "Email is required";
-    } else {
-        $email = test_input($_POST["email"]);
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $emailerror = "Invalid email format";
-        }
-    }
+ 
+$name= "";
+$nameerror= "";
+ 
+if (empty ($_POST["name"]))
+{
+$nameerror="Name is req"; // value empty
 }
-
-// CLEAN INPUT FUNCTION
+else{
+$name= test_input($_POST["name"]); //clean out input
+if (!preg_match("/^[a-zA-Z ]*$/",$name))  // only letter and middle space
+{
+    $nameerror ="Only letters and white space allowed";
+}
+ 
+}
+ 
 function test_input($data)
 {
-    $data = trim($data);
-    return $data;
+$data = trim($data); // trim amr previous data remove kore dai
+return $data;
 }
+ 
 ?>
-
+ 
+ 
 <form method="post" action="">
-    
-    Name: 
-    <input type="text" name="name" value="<?php echo $name; ?>">
-    <span style="color:red;"><?php echo $nameerror; ?></span>
-    <br><br>
-
-    Age: 
-    <input type="number" name="age" value="<?php echo $age; ?>">
-    <span style="color:red;"><?php echo $ageerror; ?></span>
-    <br><br>
-
-    Email:
-    <input type="text" name="email" value="<?php echo $email; ?>">
-    <span style="color:red;"><?php echo $emailerror; ?></span>
-    <br><br>
-
-    <input type="submit" name="submit" value="Submit">
+ 
+Name: <input type="text" name="name" value="<?php echo $name;?>">
+<?php echo $nameerror; ?>
+ 
+Age: <input type="number" name="age" >
+ 
+Email : <input type="text" name="email">
+ 
+ 
+<input type="submit" name="submit" value="Submit">
 </form>
-
+ 
+ 
 <?php
-// ONLY DISPLAY IF NO ERRORS
-if ($_SERVER["REQUEST_METHOD"] == "POST" && empty($nameerror) && empty($ageerror) && empty($emailerror)) {
-
-    echo "<h3>Your Input: </h3>";
-    echo "Name: " . $name . "<br>";
-    echo "Age: " . $age . "<br>";
-    echo "Email: " . $email . "<br>";
+if($_SERVER["REQUEST_METHOD"]== "POST" && empty($nameerror))
+{
+echo "<h3> Your Input: </h3>";
+echo "Name: ".$name. "<br>";
+ 
 }
+//g
+ 
 ?>
-
+ 
+ 
+ 
+ 
 </body>
 </html>
